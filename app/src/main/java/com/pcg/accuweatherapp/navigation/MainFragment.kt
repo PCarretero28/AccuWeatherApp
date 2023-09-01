724e615c3563ce0c08de4ed93003d9cda1853f9d
pcarreterog <pablosocialespla@gmail.com>
2023-09-01 10:59:13 +0200

    Fragments implemented

--- app/src/main/java/com/pcg/accuweatherapp/navigation/MainFragment.kt
+++ app/src/main/java/com/pcg/accuweatherapp/navigation/MainFragment.kt
@@ -6,8 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.pcg.accuweatherapp.R
+import com.pcg.accuweatherapp.databinding.ActivityMainBinding
+import com.pcg.accuweatherapp.model.location.LocationModel
+import com.pcg.accuweatherapp.service.LocationService
+import com.pcg.accuweatherapp.viewmodel.LocationViewModel
 
 class MainFragment : Fragment() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var locationViewModel: LocationViewModel
+    private lateinit var location: LocationModel
+    private var keyLocation: String = "a"
+    private val locationService: LocationService = LocationService()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
